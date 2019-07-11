2a551e0f2630fd4ebb9c0b40d993629656bdce47
GT Liu <liugoldent@gmail.com>
2019-07-11 19:30:50 +0800

    renew fontsize

--- profile.js
+++ profile.js
@@ -2,3 +2,8 @@ var w = $(window).width();
 console.log(w);
 //$('#navTitle').css('width', w);
 $('.row').css('width', w);
+$('.section2_context>p').css('font-size', '20px');
+$('.section3_context>h3').css('font-size','43px');
+$('.section3_context>p').css('font-size', '25px');
+$('.card').css('font-size', '18px');
+
